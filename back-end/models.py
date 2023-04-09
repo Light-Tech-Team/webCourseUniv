ea4c0a5682f64b7c069cbaf4c068ac60feca0093
Dia-crypto-eng <rnoir24@gmail.com>
2023-04-09 10:21:17 +0100

    create speciality & module model

--- back-end/models.py
+++ back-end/models.py
@@ -2,18 +2,28 @@ from extensions import db
 
 
 
-class Product(db.Model):
-    _tablename_ = 'product'
+class Speciality (db.Model):
+    _tablename_ = 'speciality'
     id = db.Column(db.Integer, primary_key=True)
-    NAME = db.Column(db.String(255))
-    CODE = db.Column(db.String(255))
-    PRICE_BUY = db.Column(db.Float)
-    PRICE_SELL = db.Column(db.Float)
+    nameSpeciality = db.Column(db.String(255))
+    degree = db.Column(db.Integer)
     #description = db.Column(db.String(255))
-    #image = db.Column(db.String(255))
-    ID_FAMILY = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Product %r>' % self.id
+        return '<Speciality %r>' % self.id
+    
+    
+    
+class Module (db.Model):
+    _tablename_ = 'module'
+    id = db.Column(db.Integer, primary_key=True)
+    nameModule = db.Column(db.String(255))
+    semester = db.Column(db.Integer)
+    idSpeciality = db.Column(db.Integer)
+    #description = db.Column(db.String(255))
+    
+
+    def __repr__(self):
+        return '<Module %r>' % self.id
 
 
