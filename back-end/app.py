d64053da90040a120f3700edabeb6167b8502a29
Dia-crypto-eng <rnoir24@gmail.com>
2024-10-12 02:02:40 +0100

    Enable CORS support

--- back-end/app.py
+++ back-end/app.py
@@ -5,14 +5,22 @@ from extensions import *
 db.init_app(app)
 
 with app.app_context():
-   # db.create_all()
+    db.create_all()
     pass
 
 
-from modules.controller import Speciality_id,SpecialityModule_id
+from modules.controller import Speciality_id,SpecialityModule_id,CoursController
  
 api.add_resource(Speciality_id, '/speciality/<int:deg>')  
 api.add_resource(SpecialityModule_id, '/modules/<int:spe>')
+api.add_resource(CoursController, '/course')
+
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#   return response
 
 if __name__ == "__main__":
         app.run(debug=True)
--- back-end/extensions.py
+++ back-end/extensions.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from flask_cors import CORS
-
+from flask_cors import CORS,cross_origin
 db = SQLAlchemy()
 app = Flask(__name__)
 
 api = Api(app)
+
+# CORS(app, origins="http://127.0.0.1:5000", allow_headers=[
+#         "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+#         supports_credentials=True)
+
+# CORS(app, resources={r"/" : {"origins" : "http://localhost:5000*"}})
 CORS(app)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///webCourseUniv_database.db'
